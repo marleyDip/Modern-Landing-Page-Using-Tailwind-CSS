58ba46207cec9749acffd119a2199b0e4cdb9704
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-06-26 23:55:10 +0600

    testimonial update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ const navMenu = document.querySelector("#nav_menu");
 navTriggerBtn.addEventListener("click", () => {
   navMenu.classList.toggle("nav-is-open");
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  load: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
