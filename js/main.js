b09660c3624d5cc8eaeb58cf2605f276d81b7919
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-06-27 01:59:18 +0600

    Testimonial & Brands

--- js/main.js
+++ js/main.js
@@ -7,9 +7,23 @@ navTriggerBtn.addEventListener("click", () => {
 
 // swiper
 const swiper = new Swiper(".swiper", {
-  load: true,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  //breakpoints
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    960: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
   },
 });
